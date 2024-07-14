8d1d37229e3c4d74e052c068d754ab362d00cdc3
Matevž Jaušovec <mj76786@student.uni-lj.si>
2024-07-14 19:35:36 +0200

    dodal top_decks csv in začel 3. del; string --> deck list(dict s seznami)

--- html_scraper.py
+++ html_scraper.py
@@ -68,7 +68,7 @@ def tier_list_reader(string):
             rang = "2."
         elif float(par[1]) >= 3:
             rang = "3."
-        nov_sez.append({"ime strategije": par[0],"moč": float(par[1]),"stopnja": rang})
+        nov_sez.append({"ime strategije": par[0],"moč": par[1],"stopnja": rang})
     return nov_sez
 
 # prilični je zaporedje pojavetve v html datoteki že razvrščeno po moči
@@ -81,12 +81,75 @@ def tier_list_reader(string):
 # save_frontpage(html_top_decks, mapa_podatkov, file_top_decks)
 
 def top_decks_reader(string):
+    '''spodnji preprosti vzorec ima nekaj nezaželjenih zadetkov na začetku, 
+    zato niz skrašamo po sponjem vzorcu, ki ima natanko en zadetek'''
     new_string = string.split(r'mn mt-1 is-full-tablet safar')[1]
     vzorec = r'label svelte-1w4psuu padding">(?P<ime_strategije>.+?)</div>.+?bottom-sub-label svelte-1w4psuu">(?P<število_zbirov>\d+)</div></div>'
-    sez = [pojavitev.groupdict() for pojavitev in re.finditer(vzorec, new_string, flags=re.DOTALL)]
-    return sez
-    
-# top_decks = top_decks_reader(read_file_to_string(mapa_podatkov, file_top_decks))
+    sez = re.findall(vzorec, new_string, flags=re.DOTALL)
+    nov_sez = []
+    for par in sez:
+        nov_sez.append({"ime strategije": par[0],"število zbirov": par[1]})
+    return nov_sez
 
+    
+top_decks = top_decks_reader(read_file_to_string(mapa_podatkov, file_top_decks))
+write_csv(["ime strategije","število zbirov"], top_decks, mapa_podatkov, "top_decks.csv  ")
 
 # že razvrščeno po številu objavljenih seznamov za strategijo
+
+# 3. del: ekstahiranje zbirov kart
+'''vsi zbiri (~350) so (na videz neurejeno) zbrani na koncu top_decks.html. Ideja je vse zbire shraniti v datoteke (po možnosti .ydk) 
+in nato imena teh datotek +kakšne ekstra podatke v csv'''
+
+def get_decks_string():
+    '''0 - deck id, 1 - avtor, 2 - zbirka(main, extrs, side), 3 - strategija '''
+    vse = read_file_to_string(mapa_podatkov, file_top_decks)
+    krajse = re.findall(r'ted\[\$gte\]=\(days-14.+?</script>', vse)
+    vzorec = r'\\"_id\\":\\"(.+?)\\",\\"author.+?username\\":\\"(.+?)".+?("main\\".+?"extra\\".+?"side\\".+?\]),\\"url.+?deckType\\":\{\\"name\\":\\"(.+?)\\"'
+    sez = re.findall(vzorec, krajse[0])
+    return sez
+
+def tuple_popravjalec(nterica,i=0):
+    '''odstrani odvečne backslashe'''
+    sez = list(nterica)
+    popravil = nterica[i].replace("\\", "")
+    sez[i] = popravil
+    return tuple(sez)
+
+def string_to_deck(string):
+    '''pretvori sez[x][2] iz zgornje funkcije v uporabnejšo obliko'''
+    vzorec_split = r'\\"extra\\":|\\"side\\":'
+    deck_parts = re.split(vzorec_split, string)
+    deck = {"main": [], "extra": [], "side": []}
+    vzorec = r'\\"name\\":\\"(.+?)\\"\},\\"amount\\":(\d)'
+    cards_main = re.findall(vzorec,  deck_parts[0])
+    cards_extra = re.findall(vzorec,  deck_parts[1])
+    cards_side = re.findall(vzorec,  deck_parts[2])
+
+    for i in range(len(cards_main)):
+        cards_main[i] = tuple_popravjalec(cards_main[i])
+    for i in range(len(cards_extra)):
+        cards_extra[i] = tuple_popravjalec(cards_extra[i])
+    for i in range(len(cards_side)):
+        cards_side[i] = tuple_popravjalec(cards_side[i])
+
+
+    deck["main"] = cards_main
+    deck["extra"] = cards_extra
+    deck["side"] = cards_side
+    return deck
+
+mapa_zbirk = "podatki\zbirke"
+
+def decks_to_files_and_cvs(sez,directory=mapa_zbirk)):
+    os.makedirs(directory, exist_ok=True)
+    
+
+# def make_decks_file(zbirka, directory):
+
+
+#     filename = zbirka[0] + ".text"
+#     os.makedirs(directory, exist_ok=True)
+#     path = os.path.join(directory, filename)
+#     deck = zbirka[2]
+#     with open(path, 'w', encoding='utf-8') as text_file:
